a0041bf5684a32e5c5a187e1dd432a64765e619c
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-04-28 02:22:21 -0500

    feat: client many to many relation

---
+++ app/Models/Client.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['code', 'name', 'email'];
+
+    public function programs(){
+        return $this->belongsToMany(Program::class, 'clients_programs')->withTimestamps();
+    }
+}
