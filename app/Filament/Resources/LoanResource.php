7635596fc6b0e7bd729af4052cf41606ebaa9701
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-04-29 22:00:39 -0500

    feat: add sections to loan resouurce

--- app/Filament/Resources/LoanResource.php
+++ app/Filament/Resources/LoanResource.php
@@ -4,109 +4,79 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\LoanResource\Pages;
 use App\Filament\Resources\LoanResource\RelationManagers;
+use App\Models\Classroom;
+use App\Models\Client;
 use App\Models\Loan;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
 
 class LoanResource extends Resource
 {
     protected static ?string $model = Loan::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-ticket';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\Wizard::make([
-                    Forms\Components\Wizard\Step::make('Client')
-                        ->schema([
-                            Forms\Components\Select::make('client_id')
-                                ->relationship('client', 'name')
-                                ->searchable()
-                                ->preload()
-                                ->createOptionForm([
-                                    Forms\Components\TextInput::make('code')
-                                        ->numeric()
-                                        ->required(),
-                                    Forms\Components\TextInput::make('name')
-                                        ->required(),
-                                    Forms\Components\TextInput::make('email')
-                                        ->email()
-                                        ->required(),
-                                    Forms\Components\Select::make('programs')
-                                        ->relationship('programs', 'name')
-                                        ->multiple()
-                                        ->searchable()
-                                        ->preload()
-                                ])
-                                ->required(),
-                            Forms\Components\Select::make('program_id')
-                                ->relationship('program', 'name')
-                                ->required()
-                                ->searchable()
-                                ->preload(),
-                        ]),
-                    Forms\Components\Wizard\Step::make('Location')
-                        ->schema([
-                            Forms\Components\Select::make('classroom_id')
-                                ->relationship('classroom', 'name')
-                                ->required()
-                                ->searchable()
-                                ->preload(),
-                        ]),
-                    Forms\Components\Wizard\Step::make('Devices')
-                        ->schema([
-                            Forms\Components\Repeater::make('devices')
-                                ->schema([
-                                    Forms\Components\Select::make('device_id')
-                                        ->relationship('device', 'name')
-                                        ->required()
-                                        ->searchable()
-                                        ->preload(),
-                                ])
-                                ->required()
-                                ->addActionLabel('Add device')
-                                ->columnSpanFull(),
-                            Forms\Components\Textarea::make('observations')
-                                ->nullable()
-                                ->maxLength(255),
-                        ]),
-                    Forms\Components\Wizard\Step::make('Confirmation')
-                        ->schema([])
-                ])
+                Forms\Components\Section::make('Client information')
+                    ->schema([
+                        Forms\Components\Select::make('program_id')
+                            ->relationship('program', 'name')
+                            ->preload()
+                            ->searchable()
+                            ->live()
+                            ->required(),
+                        Forms\Components\Select::make('client_id')
+                            ->relationship('client', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                    ]),
+
+                Forms\Components\Section::make('Location information')
+                ->schema([
+                    Forms\Components\Select::make('campus_id')
+                        ->relationship('campus', 'name')
+                        ->preload()
+                        ->searchable()
+                        ->live()
+                        ->required(),
+                    Forms\Components\Select::make('classroom_id')
+                        ->label('Classroom')
+                        ->options(fn (Get $get) => Classroom::query()->where('campus_id', $get('campus_id'))->pluck('name', 'id'))
+                        ->preload()
+                        ->searchable()
+                        ->required(),
+                ]),
+
+
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('loan_date')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('expected_return_date')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('return_date'),
+                Forms\Components\TextInput::make('activity')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('observations')
                     ->columnSpanFull(),
-
-
-                // Forms\Components\Select::make('status')
-                //     ->options([
-                //         'Prestado' => 'Prestado',
-                //         'Devuelto' => 'Devuelto',
-                //     ])
-                //     ->required(),
-                // Forms\Components\DatePicker::make('loan_date')
-                //     ->required(),
-                // Forms\Components\DatePicker::make('return_date'),
-
-                // Forms\Components\TextInput::make('activity')
-                //     ->nullable(),
-
-
-                // Forms\Components\Select::make('User_id')
-                //     ->relationship('user', 'name')
-                //     ->searchable()
-                //     ->preload()
-                //     ->required(),
-
-                // Forms\Components\Select::make('User_id_return')
-                //     ->relationship('user', 'name')
-                //     ->searchable()
-                //     ->preload(),
+                Forms\Components\TextInput::make('loaned_by')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('received_by')
+                    ->numeric()
+                    ->default(null),
             ]);
     }
 
@@ -114,21 +84,44 @@ class LoanResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('client.name')
-                    ->label('Client')
-                    ->searchable()
+                Tables\Columns\TextColumn::make('program_id')
+                    ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('program.name')
-                    ->label('Program'),
-                Tables\Columns\TextColumn::make('status')
-                    ->badge()
+                Tables\Columns\TextColumn::make('client_id')
+                    ->numeric()
                     ->sortable(),
+                Tables\Columns\TextColumn::make('campus_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('classroom_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
                 Tables\Columns\TextColumn::make('loan_date')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('expected_return_date')
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('return_date')
                     ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('activity')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('loaned_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('received_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
                     ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
